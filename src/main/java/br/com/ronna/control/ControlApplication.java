ca028d68bbd3e0ba58065c06ff53d404554e2225
Theo <ronna88@gmail.com>
2024-11-05 15:19:58 -0300

    fix: update yml, and principal class to force UTC

--- src/main/java/br/com/ronna/control/ControlApplication.java
+++ src/main/java/br/com/ronna/control/ControlApplication.java
@@ -3,10 +3,18 @@ package br.com.ronna.control;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ControlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ControlApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 }
